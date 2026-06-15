1f16452a4dc8a30e77ec8d8e9bb0480c9085576e
Atreyu Williams <126330688+Atrxyuu@users.noreply.github.com>
2026-06-15 17:19:32 +0100

    Updated attempt

--- tests/test_age_verification.py
+++ tests/test_age_verification.py
@@ -5,3 +5,11 @@ def test_date_of_birth_is_date_type():
 	date_of_birth = age_verification("2010-05-15")
 	assert isinstance(date_of_birth, date)
 # This test will no longer work
+
+def test_underaged_denied_access():
+	date_of_birth = age_verification("2010-05-15")
+	result = date_of_birth
+	assert result == "You are 12, you have to be 16, access denied!"
+
+def test_16_or_over_acess_granted():
+	assert result == "Access Granted!"
